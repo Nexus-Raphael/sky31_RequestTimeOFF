7268c4fde62b0e6399c35ac065f44376146b0154
LUO KUN <raphael.kun.luo@outlook.com>
2025-02-08 10:15:08 +0800

    fix:加强弱密钥

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,12 @@ from app.admin import admin_bp
 from app.user import user_bp
 from app.config import Config
 from app.database import get_connection
-
+import secrets
 app = Flask(__name__)
+app.secret_key=secrets.token_urlsafe(64)
 app.config.from_object(Config)
+
+
 
 # 注册管理员蓝图
 app.register_blueprint(admin_bp, url_prefix='/admin')
